269ec1cc60a4dfdb33b7c6dc938c2e15c2a2ac13
CLOWNAKIO <dead.kanji@mail.ru>
2023-12-08 18:40:46 +0300

    build: configured deployment files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/simbirsoft-test-task/",
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
     },
